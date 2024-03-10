1bde293679cf47857a891f6e53129fae2b53067e
cookar <87080540+ppd-abui@users.noreply.github.com>
2024-03-10 23:53:52 +0800

    neovim configuration

---
+++ nvim/lua/keymaps.lua
@@ -0,0 +1,47 @@
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- leader key 为空
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- define common options
+local opt = {
+  noremap = true,
+  silent = true,
+}
+
+
+-----------------
+-- Normal mode --
+-----------------
+
+-- Better window navigation
+vim.keymap.set('n', '<C-h>', '<C-w>h', opts)
+vim.keymap.set('n', '<C-j>', '<C-w>j', opts)
+vim.keymap.set('n', '<C-k>', '<C-w>k', opts)
+vim.keymap.set('n', '<C-l>', '<C-w>l', opts)
+
+-- Resize with arrows
+-- delta: 2 lines
+vim.keymap.set('n', '<C-Up>', ':resize -2<CR>', opts)
+vim.keymap.set('n', '<C-Down>', ':resize +2<CR>', opts)
+vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>', opts)
+vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', opts)
+
+-----------------
+-- Visual mode --
+-----------------
+
+
+-----------------
+-- Insert mode --
+-----------------
+
+-- Escape insert mode
+vim.keymap.set('i', 'jk', '<Esc>', opts)
